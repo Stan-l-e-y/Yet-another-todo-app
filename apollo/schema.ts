5bb8d26dcc6472ccda63b1d250bd78a6a0c5b81b
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-11-05 18:47:49 -0400

    some apollo setup

---
+++ apollo/schema.ts
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client';
+
+const typeDefs = gql`
+    type: Account {
+        id: ID!
+        userId: ID!
+        type: String!
+        provider: String!
+        providerAccountId: String!
+        refreshToken: String
+        accessToken: String
+        expires_at: Int
+        token_type: String
+        scope: String
+        id_token: String
+        session_state: String
+    }
+
+    type: Session {
+        id: ID!
+        sessionToken: String!
+        userId: ID!
+        expires: Date!
+    }
+
+    type: User {
+        id: ID!
+        name: String
+        email: String
+        emailVerified: Date
+        image: String
+        accounts: [Account]
+        sessions: [Session]
+        todos: [Todo]
+    }
+
+    type: Todo {
+        id: ID!
+        userId: ID!
+        title: String!
+        completed: Boolean!
+        createdAt: Date!
+        updatedAt: Date!
+        scheduled: Boolean!
+        scheduledDate: Date
+    }
+`;
+
+export default typeDefs;
